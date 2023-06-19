61c7f64a0d0e8e5156d935904ca35d44e4861c2c
sowmiya <sowmiyanallasivam3434@gmail.com>
2023-06-19 11:19:46 +0530

    first commit

--- src/components/Oursuccess.jsx
+++ src/components/Oursuccess.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
-import s1 from '../images/s1.gif'
-import s2 from '../images/s2.gif'
-import s3 from '../images/s3.gif'
-import s4 from '../images/s4.gif'
-import s5 from '../images/s5.gif'
+import s1 from '../images/Capture-759ac002.jpg'
 function Oursuccess() {
   return (
     <div>
@@ -16,19 +12,19 @@ function Oursuccess() {
                  <p className='success-p'> Back in 2019, FliqaIndia recognize as a startup with DPIT in Startup India.</p>
              </div>
              <div className='success-main'>
-                  <img className='success-img' src={s2}/>
+                  <img className='success-img' src={s1}/>
                   <p className='success-p'> Appreciation from Startup Bengal for our creative startup community</p>
              </div>
              <div className='success-main'>
-                  <img className='success-img' src={s3}/>
+                  <img className='success-img' src={s1}/>
                   <p className='success-p'>FliqaIndia got featured on Yourstory. Click here</p>
              </div>
              <div className='success-main'>
-                  <img className='success-img' src={s4}/>
+                  <img className='success-img' src={s1}/>
                   <p className='success-p'>Received awards from ImagesBazaar on leading creative industry</p>
              </div>
              <div className='success-main'>
-                  <img className='success-img' src={s5}/>
+                  <img className='success-img' src={s1}/>
                   <p className='success-p'> Top ten in Intuit Circle Codeathon, 2020</p>
              </div>
              </div>
